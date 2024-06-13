9c8671bc88246f6a0290486ca4ff3ff1d0e79b1a
DuyDirox <duy.nguyennhat@dirox.net>
2024-06-13 15:55:41 +0700

    up

---
+++ 7.Sort/selectionSort.js
@@ -0,0 +1,24 @@
+// Javascript program for implementation of selection sort
+
+function swap(arr, i, j) {
+    let tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
+
+function selectionSort(arr){
+    for(let i =0; i < arr.length - 1; i++){
+        var minIdx = i;
+        for(let j = i+1; j< arr.length; j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j
+            }
+        }
+        swap(arr, i, minIdx)
+
+    }
+}
+
+let arr = [5, 4, 3, 2, 1]
+selectionSort(arr)
+console.log(arr)
